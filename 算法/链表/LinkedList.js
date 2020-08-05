3737194201b10c8bd94a07c7e6c4dba9d1e2013b
fbm <wuq_xd@163.com>
2020-08-05 16:12:25 +0800

    【算法】【链表】链表的js实现 - 链表基本操作方法

--- 算法/链表/LinkedList.js
+++ 算法/链表/LinkedList.js
@@ -1,6 +1,7 @@
 // 链表的js实现
 function LinkedList() {
-  // 链表结点
+
+  // 链表结点构造函数
   let Node = function(el) {
     this.el = el // 数据域
     this.next = null // 指针域
@@ -8,6 +9,15 @@ function LinkedList() {
 
   let length = 0 // 链表长度
   let head = null // 头结点
+
+  // 查找节点
+  this.find = function(el) {
+    let currentNode = head
+    while(currentNode && currentNode.el !== el) {
+      currentNode = currentNode.next
+    }
+    return currentNode
+  }
 
   // 尾新增节点
   this.append = function(el) {
@@ -30,11 +40,65 @@ function LinkedList() {
   }
 
   // 任意位置插入
-  this.insert = function(position, el) {
-
+  this.insert = function(newEl, el) {
+    const currentNode = this.find(el)
+    if (!currentNode) return false
+    const node = new Node(newEl)
+    node.next = currentNode.next
+    currentNode.next = node
+    length++
+    return node
   }
 
+  // 更新结点数据
+  this.update = function(el, newEl) {
+    const node = this.find(el)
+    node.el = newEl
+    return node
+  }
+
+  // 查找前驱结点
+  this.findPrevious = function(el) {
+    let current = head
+    while(true) {
+      if (current.next === null) {
+        current = false
+        break
+      }
+      if (current.next.el === el) {
+        break
+      }
+      current = current.next
+    }
+    return current
+  }
+
+  // 删除结点
+  this.remove = function(el) {
+    const node = this.find(el)
+    if(!node) return false
+    if (el === head.el) {
+      head = head.next
+      length--
+      return
+    }
+    const previous = this.findPrevious(el)
+    previous.next = node.next
+    length--
+    return node
+  }
+
+  // 返回头结点
   this.getHead = function () {
     return head
+  }
+
+  // 打印结点数据域
+  this.print = function() {
+    let current = head
+    while(current.next) {
+      console.log(current.el)
+      current = current.next
+    }
   }
 }
